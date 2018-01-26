3ffed900feed67a19c203e40def8fb728327ae86
Scott Mulder <scott@treefortbikes.com>
2018-01-25 23:35:22 -0500

    Check if file exists on GCS to avoid writing unnecessary files

---
+++ src/util/getGCSUrlIfExists.js
@@ -0,0 +1,31 @@
+const generateGCSUrl = require('./generateGCSUrl')
+
+module.exports = async function getGCSUrlIfExists(params) {
+	return new Promise((resolve, reject) => {
+		const { Bucket, Key, contentType, reader, staticUrl, bucketDir, type, storage } = params;
+
+		let locBucketDir = ''
+		if(bucketDir){
+			locBucketDir = bucketDir + '/'
+		}
+
+		storage
+		.bucket(Bucket)
+		.file(locBucketDir + Key)
+		.exists()
+		.then((result) => {
+
+			if(result && result[0]){
+				resolve(generateGCSUrl(Bucket, Key, staticUrl, locBucketDir));
+			}else{
+				//console.log('file: ' + staticUrl + '/' + locBucketDir + '/' + Key + ' ----- did not exist');
+				resolve(null);
+			}
+		}).catch((e) => {
+			console.log('Error while checking if file exists in bucket: ', e);
+			//reject(e);
+		});
+
+
+	})
+}
